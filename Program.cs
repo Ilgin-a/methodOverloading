880c3088fae0cd539e1a5c5d5be20fcbd1f9a6a5
Ilgina <88985657+Ilgin-a@users.noreply.github.com>
2022-05-02 13:04:33 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Out parametreler
+string sayi = "999";
+
+bool sonuc = int.TryParse(sayi, out int outSayi);
+if (sonuc)
+{
+    Console.WriteLine("Başarılı!");
+    Console.WriteLine(outSayi);
+}
+else
+{
+    Console.WriteLine("Başarısız!");
+}
+
+Metotlar instance = new Metotlar();
+instance.Topla(4, 5, out int toplamSonucu);
+Console.WriteLine(toplamSonucu);
+
+//Metot Aşırı Yükleme - Overloading
+int ifade = 999;
+instance.EkranaYazdir(Convert.ToString(ifade));
+instance.EkranaYazdir(ifade);
+instance.EkranaYazdir("Ilgın", "Arpacı");
+
+//Metot İmzası
+//Metot adı + parametre sayısı + parametre
+
+class Metotlar
+{
+    public void Topla(int a, int b, out int toplam)
+    {
+        toplam = a + b;
+    }
+    public void EkranaYazdir(string veri)
+    {
+        Console.WriteLine(veri);
+    }
+    public void EkranaYazdir(string veri1, string veri2)
+    {
+        Console.WriteLine(veri1 + veri2);
+    }
+}
